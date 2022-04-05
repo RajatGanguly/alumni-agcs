1815a6f9ac6b3fbc6a80f5b684dc678d2664d6f0
RAJAT <admin@selflerner.org>
2022-04-05 22:58:56 +0530

    Basic layout is ready

---
+++ members.php
@@ -0,0 +1,278 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MEMBERS | AGSC</title>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.js"></script>
+    <link rel="stylesheet" href="styles/navbar.css">
+    <link rel="stylesheet" href="styles/members.css">
+    <link rel="stylesheet" href="styles/footer.css">
+</head>
+<body>
+    <?php
+        include "./components/navbar.php";
+    ?>
+    <br><br><br>
+    <br><br>
+    <div class="d-grid gap-2 col-6 mx-auto">
+        <button class="btn btn-info" type="button"><a href="./add-member.php" class="applybtn">Apply For Membership</a></button>
+    </div>
+    <br><br>
+    <div class="container">
+    <p class="csubhead">Life Members:</p>
+        <table id="lmembertbl">
+            <thead>
+                <tr>
+                    <th>NAME</th>
+                    <th>MEMBERSHIP NO.</th>
+                    <th>BATCH</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Tony Stark</td>
+                    <td>AGSC/ALUM/0001</td>
+                    <td>1950 - 1950</td>
+                </tr>
+                <tr>
+                    <td>Steve Rogers</td>
+                    <td>AGSC/ALUM/0002</td>
+                    <td>1960 - 1964</td>
+                </tr>
+                <tr>
+                    <td>Thor Odinson</td>
+                    <td>AGSC/ALUM/0003</td>
+                    <td>1955 - 1959</td>
+                </tr>
+                <tr>
+                    <td>Tony Stark</td>
+                    <td>AGSC/ALUM/0001</td>
+                    <td>1950 - 1950</td>
+                </tr>
+                <tr>
+                    <td>Steve Rogers</td>
+                    <td>AGSC/ALUM/0002</td>
+                    <td>1960 - 1964</td>
+                </tr>
+                <tr>
+                    <td>Thor Odinson</td>
+                    <td>AGSC/ALUM/0003</td>
+                    <td>1955 - 1959</td>
+                </tr>
+                <tr>
+                    <td>Tony Stark</td>
+                    <td>AGSC/ALUM/0001</td>
+                    <td>1950 - 1950</td>
+                </tr>
+                <tr>
+                    <td>Steve Rogers</td>
+                    <td>AGSC/ALUM/0002</td>
+                    <td>1960 - 1964</td>
+                </tr>
+                <tr>
+                    <td>Thor Odinson</td>
+                    <td>AGSC/ALUM/0003</td>
+                    <td>1955 - 1959</td>
+                </tr>
+                <tr>
+                    <td>Tony Stark</td>
+                    <td>AGSC/ALUM/0001</td>
+                    <td>1950 - 1950</td>
+                </tr>
+                <tr>
+                    <td>Steve Rogers</td>
+                    <td>AGSC/ALUM/0002</td>
+                    <td>1960 - 1964</td>
+                </tr>
+                <tr>
+                    <td>Thor Odinson</td>
+                    <td>AGSC/ALUM/0003</td>
+                    <td>1955 - 1959</td>
+                </tr>
+                <tr>
+                    <td>Tony Stark</td>
+                    <td>AGSC/ALUM/0001</td>
+                    <td>1950 - 1950</td>
+                </tr>
+                <tr>
+                    <td>Steve Rogers</td>
+                    <td>AGSC/ALUM/0002</td>
+                    <td>1960 - 1964</td>
+                </tr>
+                <tr>
+                    <td>Thor Odinson</td>
+                    <td>AGSC/ALUM/0003</td>
+                    <td>1955 - 1959</td>
+                </tr>
+                <tr>
+                    <td>Tony Stark</td>
+                    <td>AGSC/ALUM/0001</td>
+                    <td>1950 - 1950</td>
+                </tr>
+                <tr>
+                    <td>Steve Rogers</td>
+                    <td>AGSC/ALUM/0002</td>
+                    <td>1960 - 1964</td>
+                </tr>
+                <tr>
+                    <td>Thor Odinson</td>
+                    <td>AGSC/ALUM/0003</td>
+                    <td>1955 - 1959</td>
+                </tr>
+                <tr>
+                    <td>Tony Stark</td>
+                    <td>AGSC/ALUM/0001</td>
+                    <td>1950 - 1950</td>
+                </tr>
+                <tr>
+                    <td>Steve Rogers</td>
+                    <td>AGSC/ALUM/0002</td>
+                    <td>1960 - 1964</td>
+                </tr>
+                <tr>
+                    <td>Thor Odinson</td>
+                    <td>AGSC/ALUM/0003</td>
+                    <td>1955 - 1959</td>
+                </tr>
+            </tbody>
+        </table>
+    <br><br><br><br><br><br>
+    <p class="csubhead">Annual Members:</p>
+        <table id="amembertbl">
+            <thead>
+                <tr>
+                    <th>NAME</th>
+                    <th>MEMBERSHIP NO.</th>
+                    <th>BATCH</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Tony Stark</td>
+                    <td>AGSC/ALUM/0001</td>
+                    <td>1950 - 1950</td>
+                </tr>
+                <tr>
+                    <td>Steve Rogers</td>
+                    <td>AGSC/ALUM/0002</td>
+                    <td>1960 - 1964</td>
+                </tr>
+                <tr>
+                    <td>Thor Odinson</td>
+                    <td>AGSC/ALUM/0003</td>
+                    <td>1955 - 1959</td>
+                </tr>
+                <tr>
+                    <td>Tony Stark</td>
+                    <td>AGSC/ALUM/0001</td>
+                    <td>1950 - 1950</td>
+                </tr>
+                <tr>
+                    <td>Steve Rogers</td>
+                    <td>AGSC/ALUM/0002</td>
+                    <td>1960 - 1964</td>
+                </tr>
+                <tr>
+                    <td>Thor Odinson</td>
+                    <td>AGSC/ALUM/0003</td>
+                    <td>1955 - 1959</td>
+                </tr>
+                <tr>
+                    <td>Tony Stark</td>
+                    <td>AGSC/ALUM/0001</td>
+                    <td>1950 - 1950</td>
+                </tr>
+                <tr>
+                    <td>Steve Rogers</td>
+                    <td>AGSC/ALUM/0002</td>
+                    <td>1960 - 1964</td>
+                </tr>
+                <tr>
+                    <td>Thor Odinson</td>
+                    <td>AGSC/ALUM/0003</td>
+                    <td>1955 - 1959</td>
+                </tr>
+                <tr>
+                    <td>Tony Stark</td>
+                    <td>AGSC/ALUM/0001</td>
+                    <td>1950 - 1950</td>
+                </tr>
+                <tr>
+                    <td>Steve Rogers</td>
+                    <td>AGSC/ALUM/0002</td>
+                    <td>1960 - 1964</td>
+                </tr>
+                <tr>
+                    <td>Thor Odinson</td>
+                    <td>AGSC/ALUM/0003</td>
+                    <td>1955 - 1959</td>
+                </tr>
+                <tr>
+                    <td>Tony Stark</td>
+                    <td>AGSC/ALUM/0001</td>
+                    <td>1950 - 1950</td>
+                </tr>
+                <tr>
+                    <td>Steve Rogers</td>
+                    <td>AGSC/ALUM/0002</td>
+                    <td>1960 - 1964</td>
+                </tr>
+                <tr>
+                    <td>Thor Odinson</td>
+                    <td>AGSC/ALUM/0003</td>
+                    <td>1955 - 1959</td>
+                </tr>
+                <tr>
+                    <td>Tony Stark</td>
+                    <td>AGSC/ALUM/0001</td>
+                    <td>1950 - 1950</td>
+                </tr>
+                <tr>
+                    <td>Steve Rogers</td>
+                    <td>AGSC/ALUM/0002</td>
+                    <td>1960 - 1964</td>
+                </tr>
+                <tr>
+                    <td>Thor Odinson</td>
+                    <td>AGSC/ALUM/0003</td>
+                    <td>1955 - 1959</td>
+                </tr>
+                <tr>
+                    <td>Tony Stark</td>
+                    <td>AGSC/ALUM/0001</td>
+                    <td>1950 - 1950</td>
+                </tr>
+                <tr>
+                    <td>Steve Rogers</td>
+                    <td>AGSC/ALUM/0002</td>
+                    <td>1960 - 1964</td>
+                </tr>
+                <tr>
+                    <td>Thor Odinson</td>
+                    <td>AGSC/ALUM/0003</td>
+                    <td>1955 - 1959</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    <br><br><br>
+    <div class="d-grid gap-2 col-6 mx-auto">
+        <button class="btn btn-info" type="button"><a href="./add-member.php" class="applybtn">Apply For Membership</a></button>
+    </div>
+    <br><br><br>
+    <br><br><br>
+    <?php
+        include "./components/footer.php";    
+    ?>
+</body>
+<script>
+    $(document).ready( function () {
+        $('#lmembertbl').DataTable();
+        $('#amembertbl').DataTable();
+    } );
+</script>
+</html>
